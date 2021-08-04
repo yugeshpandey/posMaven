b3a203ac029f6961362463ab146d7ac452cdfc8f
Yugesh Pandey <27809103+yugeshpandey@users.noreply.github.com>
2021-08-04 18:39:43 -0400

    Added dealer Entity

---
+++ pos/src/main/java/com/cloudmandu/pos/entity/DealerDTO.java
@@ -0,0 +1,41 @@
+package com.cloudmandu.pos.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name  = "DEALER")
+public class DealerDTO {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "DEALER")
+	private String dealerName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDealerName() {
+		return dealerName;
+	}
+
+	public void setDealerName(String dealerName) {
+		this.dealerName = dealerName;
+	}
+	
+	
+
+	
+
+}
---
+++ pos/src/main/java/com/cloudmandu/pos/models/Dealer.java
@@ -0,0 +1,23 @@
+package com.cloudmandu.pos.models;
+
+public class Dealer {
+	
+	private Long id;
+	private String dealerName;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDealerName() {
+		return dealerName;
+	}
+	public void setDealerName(String dealerName) {
+		this.dealerName = dealerName;
+	}
+	
+	
+
+}
